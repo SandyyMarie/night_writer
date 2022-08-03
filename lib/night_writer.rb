c6fc56ac1c4512d4c42da2a6d4daccc57a3138ae
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-08-02 19:57:53 -0600

    feat: able to write identical message to file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,13 +1,21 @@
-
 class NightWriter
 
     attr_reader :out_file, 
                 :read_file
 
+    attr_writer :translated_txt
+
     def initialize
-        @out_file = File.new(ARGV[1], "w")
+        @out_file = File.new(ARGV[1], "w") #maybe .open
         @read_file = File.readlines(ARGV[0])
         puts "Created '#{ARGV[1]}' containing #{@read_file.join.length} characters"
+
+        @translated_txt = []
+        write
+    end
+
+    def write
+        @out_file.write(@read_file.join) #change read_file to whatever output i want
     end
 end
 
