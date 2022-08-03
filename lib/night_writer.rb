14acae1f78735b0d5b8072beb43af499c8de9333
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-08-02 19:22:23 -0600

    feat: iteration 1 complete

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,9 @@
 out_file = File.new(ARGV[1], "w")
+read_file = File.readlines(ARGV[0])
+puts "Created '#{ARGV[1]}' containing #{read_file.join.length} characters"
+
 class NightWriter
 
     def initialize
-        require 'pry'; binding.pry
     end
 end
